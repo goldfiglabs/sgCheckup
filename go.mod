51f5e59db152deda572ecc392c677a2585c2b794
Greg Soltis <gsoltis@gmail.com>
2021-02-12 11:58:39 -0800

    WIP on adding the security group query

--- go.mod
+++ go.mod
@@ -4,14 +4,14 @@ go 1.15
 
 require (
 	github.com/aws/aws-sdk-go-v2 v1.2.0 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.1.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.1.1
 	github.com/containerd/containerd v1.4.3 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v20.10.3+incompatible
 	github.com/docker/go-connections v0.4.0
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/lib/pq v1.9.0 // indirect
+	github.com/lib/pq v1.9.0
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/phayes/freeport v0.0.0-20180830031419-95f893ade6f2
@@ -20,5 +20,5 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0 // indirect
 	google.golang.org/grpc v1.35.0 // indirect
-	k8s.io/apimachinery v0.20.2 // indirect
+	k8s.io/apimachinery v0.20.2
 )
--- main.go
+++ main.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/phayes/freeport"
 
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
 
@@ -131,7 +132,7 @@ func getDockerClient() (*client.Client, error) {
 
 const postgresRef = "supabase/postgres:0.13.0"
 const postgresPort = 5432
-const introspectorRef = "goldfig/lighthouse:latest"
+const introspectorRef = "goldfig/introspector:latest"
 
 func requireDockerImage(ctx context.Context, client *client.Client, ref string) error {
 	images, err := client.ImageList(ctx, types.ImageListOptions{
@@ -160,7 +161,12 @@ func requireDockerImage(ctx context.Context, client *client.Client, ref string) 
 const postgresContainerName = "sgCheckup-db"
 const introspectorContainerName = "sgCheckup-introspector"
 
-func createPostgresContainer(ctx context.Context, suCredential *dbCredential, dockerClient *client.Client) (*dbService, error) {
+func createPostgresContainer(
+	ctx context.Context,
+	suCredential *dbCredential,
+	dockerClient *client.Client,
+	reuseExisting bool,
+) (*dbService, error) {
 	existingContainers, err := dockerClient.ContainerList(ctx, types.ContainerListOptions{
 		Filters: filters.NewArgs(filters.Arg("name", "/"+postgresContainerName)),
 		All:     true,
@@ -168,18 +174,32 @@ func createPostgresContainer(ctx context.Context, suCredential *dbCredential, do
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to list existing containers")
 	}
+
+	postgresNatPort, err := nat.NewPort("tcp", strconv.Itoa(postgresPort))
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create postgres nat.Port")
+	}
 	if len(existingContainers) == 1 {
 		container := existingContainers[0]
+		if reuseExisting {
+			containerDesc, err := dockerClient.ContainerInspect(ctx, container.ID)
+			if err != nil {
+				return nil, errors.Wrap(err, "Failed to inspect running postgres")
+			}
+			bindings := containerDesc.HostConfig.PortBindings[postgresNatPort]
+			// TODO: pull out credential?
+			return &dbService{
+				containerService:    containerService{ContainerID: container.ID},
+				SuperUserCredential: suCredential,
+				Address:             bindings[0],
+			}, nil
+		}
 		err = stopAndRemoveContainer(ctx, dockerClient, container.ID)
 		if err != nil {
 			return nil, errors.Wrap(err, "Failed to remove existing container")
 		}
 	}
 
-	postgresNatPort, err := nat.NewPort("tcp", strconv.Itoa(postgresPort))
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to create postgres nat.Port")
-	}
 	exposedPorts := make(nat.PortSet)
 	exposedPorts[postgresNatPort] = struct{}{}
 
@@ -232,14 +252,14 @@ func createPostgresContainer(ctx context.Context, suCredential *dbCredential, do
 	}, nil
 }
 
-func runPostgres(ctx context.Context, dockerClient *client.Client, suCredential *dbCredential) (*dbService, error) {
+func runPostgres(ctx context.Context, dockerClient *client.Client, suCredential *dbCredential, reuseExisting bool) (*dbService, error) {
 	log.Info("Running postgres")
 	err := requireDockerImage(ctx, dockerClient, postgresRef)
 	if err != nil {
 		return nil, err
 	}
 
-	service, err := createPostgresContainer(ctx, suCredential, dockerClient)
+	service, err := createPostgresContainer(ctx, suCredential, dockerClient, reuseExisting)
 	if err != nil {
 		return nil, err
 	}
@@ -316,7 +336,7 @@ func createIntrospectorContainer(ctx context.Context, dockerClient *client.Clien
 
 func runIntrospector(ctx context.Context, dockerClient *client.Client, postgresService *dbService) (*introspectorService, error) {
 	log.Info("Checking for introspector image")
-	err := requireDockerImage(ctx, dockerClient, "lighthouse:latest")
+	err := requireDockerImage(ctx, dockerClient, introspectorRef)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to get instrospector docker image")
 	}
@@ -372,11 +392,141 @@ func installDbFunction(db *sql.DB) error {
 	return nil
 }
 
-func runSecurityGroupTool(pgInfo string) (interface{}, error) {
+const analysisQuery = `WITH ippermissions AS (
+	SELECT
+		SG._id,
+		bool_or(is_rfc1918block((IP.value->>'CidrIp')::cidr) = false AND masklen((IP.value->>'CidrIp')::cidr) BETWEEN 1 AND 23) AS is_large_public_block,
+		bool_and((is_rfc1918block((IP.value->>'CidrIp')::cidr) OR masklen((IP.value->>'CidrIp')::cidr) != 0)) as internal_only,
+		COUNT(*) AS range_count
+	FROM
+		aws_ec2_securitygroup AS SG
+		CROSS JOIN LATERAL jsonb_array_elements(SG.ippermissions) AS P
+		CROSS JOIN LATERAL jsonb_array_elements(P.value->'IpRanges') AS IP
+	GROUP BY SG._id
+), permissions AS (
+SELECT
+	SG._id,
+	permissions.* AS permissions
+FROM
+	aws_ec2_securitygroup AS SG
+	CROSS JOIN LATERAL jsonb_array_elements(SG.ippermissions) AS permissions
+), raw_ranges AS (
+SELECT
+	P._id,
+	P.value AS permission,
+	(R.value ->> 'CidrIp')::cidr AS cidr,
+	COALESCE(((P.value ->> 'FromPort')::int), 0) AS from_port,
+	COALESCE(((P.value ->> 'ToPort')::int), 65535) AS to_port
+FROM
+	permissions as P
+	CROSS JOIN LATERAL jsonb_array_elements(P.value->'IpRanges') AS R
+), public_tcp_ranges AS (
+SELECT
+	R._id,
+	ARRAY_AGG(int4range(R.from_port, R.to_port, '[]')) AS port_ranges
+FROM
+	raw_ranges AS R
+WHERE
+	R.from_port <= R.to_port
+	AND R.cidr = '0.0.0.0/0'::cidr
+	AND R.permission ->> 'IpProtocol' IN ('-1', 'tcp')
+GROUP BY R._id
+), security_group_attrs AS (
+SELECT
+	SG._id,
+	SG._id IN (SELECT DISTINCT(securitygroup_id) FROM aws_ec2_networkinterface_securitygroup) AS in_use,
+	SG.groupname = 'default' AS is_default,
+	R.port_ranges,
+	COALESCE(IP.is_large_public_block, false) AS is_large_public_block,
+	COALESCE(IP.range_count, 0) > 50 AS large_range_count,
+	COALESCE(IP.range_count, 0) = 0 AS is_restricted,
+	COALESCE(IP.internal_only, true) AS internal_only
+FROM
+	aws_ec2_securitygroup AS SG
+	LEFT JOIN ippermissions AS IP
+		ON SG._id = IP._id
+	LEFT JOIN public_tcp_ranges AS R
+		ON SG._id = R._id
+), publicips AS (
+	SELECT
+		SG._id,
+		ARRAY_AGG((NI.association ->> 'PublicIp')::inet) AS ips
+	FROM
+		aws_ec2_securitygroup AS SG
+		INNER JOIN aws_ec2_networkinterface_securitygroup AS NI2SG
+			ON SG._id = NI2SG.securitygroup_id
+		INNER JOIN aws_ec2_networkinterface AS NI
+			ON NI2SG.networkinterface_id = NI._id
+	WHERE
+		NI.association IS NOT NULL
+	GROUP BY
+		SG._id
+)
+SELECT
+	SG.uri AS arn,
+	SG.groupname,
+	COALESCE(P.ips, '{}') AS ips,
+	Attrs.in_use,
+	Attrs.is_default,
+	Attrs.port_ranges,
+	-- Attrs.allows_udp,
+	-- SG.unsafe_ports,
+	Attrs.is_large_public_block,
+	Attrs.large_range_count,
+	Attrs.is_restricted,
+	Attrs.internal_only
+	-- SG.ippermissions
+FROM
+	aws_ec2_securitygroup AS SG
+	LEFT JOIN security_group_attrs AS Attrs
+		ON SG._id = Attrs._id
+	LEFT JOIN publicips AS P
+		ON P._id = SG._id
+`
+
+type securityGroupRow struct {
+	arn                string
+	groupName          string
+	ips                []string
+	inUse              bool
+	isDefault          bool
+	portRanges         sql.NullString
+	isLargePublicBlock bool
+	largeRangeCount    bool
+	isRestricted       bool
+	internalOnly       bool
+}
+
+func (r *securityGroupRow) isProblematic() bool {
+	return false
+}
+
+func runSecurityGroupQuery(db *sql.DB) ([]securityGroupRow, error) {
+	rows, err := db.Query(analysisQuery)
+	if err != nil {
+		return nil, errors.Wrap(err, "DB error analyzing")
+	}
+	defer rows.Close()
+	results := make([]securityGroupRow, 0)
+	for rows.Next() {
+		row := securityGroupRow{}
+		err = rows.Scan(&row.arn, &row.groupName, pq.Array(&row.ips), &row.inUse, &row.isDefault, &row.portRanges,
+			&row.isLargePublicBlock, &row.largeRangeCount, &row.isRestricted, &row.internalOnly)
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to unmarshal a row")
+		}
+		results = append(results, row)
+	}
+	log.Infof("rows %v", results)
+	return results, nil
+}
+
+func runSecurityGroupTool(pgInfo string) ([]securityGroupRow, error) {
 	db, err := sql.Open("postgres", pgInfo)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to connect to db")
 	}
+	defer db.Close()
 	err = db.Ping()
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to ping db")
@@ -386,12 +536,74 @@ func runSecurityGroupTool(pgInfo string) (interface{}, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to install fixture functions")
 	}
-	return nil, nil
+	results, err := runSecurityGroupQuery(db)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to run analysis query")
+	}
+	return results, nil
+}
+
+type reportRow struct {
+	arn       string
+	name      string
+	status    string
+	publicIps []string
+	inUse     bool
+	isDefault bool
+	notes     []string
+}
+
+func newReportRow(queryRow *securityGroupRow) *reportRow {
+	return &reportRow{}
+}
+
+func analyzeSecurityGroupResults(results []securityGroupRow) {
+	reportRows := make([]reportRow, len(results))
+	for _, row := range results {
+		fmt.Printf("ARN %v\n", row.arn)
+		var status string
+		if row.isDefault {
+			if row.inUse {
+				if row.isRestricted || row.internalOnly || len(row.ips) == 0 {
+					status = "yellow"
+				} else {
+					status = "red"
+				}
+			} else {
+				if row.isRestricted {
+					// best case for default groups, locked down and not in use
+					status = "green"
+				} else {
+					status = "yellow"
+				}
+			}
+		} else {
+			if row.inUse {
+				if row.isRestricted || !row.isProblematic() {
+					status = "green"
+				} else if len(row.ips) == 0 {
+					status = "yellow"
+				} else {
+					status = "red"
+				}
+			} else {
+				// Not the default, so shouldn't exist if it's not in use
+				status = "yellow"
+			}
+		}
+		reportRows = append(reportRows, reportRow{
+			arn:    row.arn,
+			name:   row.groupName,
+			status: status,
+		})
+	}
 }
 
 func main() {
-	var skipIntrospector bool
+	var skipIntrospector, leavePostgresUp, reusePostgres bool
 	flag.BoolVar(&skipIntrospector, "skip-introspector", false, "Skip running an import, use existing data")
+	flag.BoolVar(&leavePostgresUp, "leave-postgres", false, "Leave postgres running in a docker container")
+	flag.BoolVar(&reusePostgres, "reuse-postgres", false, "Reuse an existing postgres instance, if it is running")
 	flag.Parse()
 
 	ctx := context.Background()
@@ -407,7 +619,7 @@ func main() {
 		username: "postgres",
 		password: "postgres",
 	}
-	postgresService, err := runPostgres(ctx, dockerClient, superuser)
+	postgresService, err := runPostgres(ctx, dockerClient, superuser, reusePostgres)
 	if err != nil {
 		panic(err)
 	}
@@ -434,12 +646,15 @@ func main() {
 			panic(err)
 		}
 	}
-	_, err = runSecurityGroupTool(postgresService.ConnectionString(importer))
+	results, err := runSecurityGroupTool(postgresService.ConnectionString(importer))
 	if err != nil {
 		panic(err)
 	}
-	/*err = postgresService.ShutDown(ctx, dockerClient)
-	if err != nil {
-		panic(err)
-	}*/
+	analyzeSecurityGroupResults(results)
+	if !leavePostgresUp {
+		err = postgresService.ShutDown(ctx, dockerClient)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
