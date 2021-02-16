deae3aeacdb3243fb9805091930250f908687e22
Greg Soltis <gsoltis@gmail.com>
2021-02-16 13:34:25 -0800

    More properly use pkger, update html template

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"strconv"
 	"strings"
 
+	"github.com/markbates/pkger"
 	"github.com/pkg/errors"
 
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -45,7 +50,64 @@ func printReportRows(rows []report.Row) {
 	}
 }
 
+type templateData struct {
+	Report report.Report
+}
+
+func writeHtmlReport(report report.Report, outputFilename string) error {
+	filename := "/templates/security_groups.gohtml"
+	f, err := pkger.Open(filename)
+	if err != nil {
+		return errors.Wrap(err, "Failed to load html template")
+	}
+	defer f.Close()
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		return errors.Wrap(err, "Failed to read html template")
+	}
+	t := template.New("sgCheckup")
+	t.Funcs(template.FuncMap{
+		"yn": func(b bool) string {
+			if b {
+				return "yes"
+			}
+			return "no"
+		},
+		"inc": func(i int) int {
+			return i + 1
+		},
+		"notes": func(s []string) string {
+			return strings.Join(s, ", ")
+		},
+		"ipList": func(ips []string) string {
+			if len(ips) == 0 {
+				return "<NONE>"
+			}
+			if len(ips) > 8 {
+				return strings.Join(ips[:8], ", ") + "...(+" + strconv.Itoa(len(ips)-8) + ")"
+			}
+			return strings.Join(ips, ", ") + " (" + strconv.Itoa(len(ips)) + ")"
+		},
+	})
+	t, err = t.Parse(string(bytes))
+	if err != nil {
+		return errors.Wrap(err, "Failed to parse template")
+	}
+	outputFile, err := os.Create(outputFilename)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to create output file %v", outputFilename)
+	}
+	defer outputFile.Close()
+	err = t.Execute(outputFile, &templateData{Report: report})
+	if err != nil {
+		return errors.Wrap(err, "Failed to run html template")
+	}
+	return nil
+}
+
 func main() {
+	pkger.Include("/templates")
+	pkger.Include("/queries")
 	var skipIntrospector, leavePostgresUp, reusePostgres bool
 	flag.BoolVar(&skipIntrospector, "skip-introspector", false, "Skip running an import, use existing data")
 	flag.BoolVar(&leavePostgresUp, "leave-postgres", false, "Leave postgres running in a docker container")
@@ -93,7 +155,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	printReportRows(report)
+	// printReportRows(report)
+	err = writeHtmlReport(report, "index.html")
+	if err != nil {
+		panic(err)
+	}
 	if !leavePostgresUp {
 		err = postgresService.ShutDown()
 		if err != nil {
