f97171683e91eb8e0be92927f78fc893ca2b999b
Greg Soltis <greg@goldfiglabs.com>
2021-08-04 16:31:01 -0700

    Build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Create output folder
+mkdir -p $DIR/dist
+
+cd $DIR
+
+# Pre-req: go get github.com/markbates/pkger/cmd/pkger
+GOBIN="${GOPATH:-${HOME}/go}"
+$GOBIN/bin/pkger
+
+GOOS=darwin GOARCH=amd64 go build -o dist/sgCheckup_darwin_amd64
+GOOS=darwin GOARCH=arm64 go build -o dist/sgCheckup_darwin_arm64
+
+GOOS=linux GOARCH=amd64 go build -o dist/sgCheckup_linux
